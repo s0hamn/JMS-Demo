afb8a60d4dd6ed278ad272392adac8a6208c5472
s0hamn <soham.naigaonkar@gmail.com>
2024-05-24 11:09:47 +0530

    added postgresql implementation for dispatch orders

--- Inventory/src/main/java/com/example/demo/services/InventoryService.java
+++ Inventory/src/main/java/com/example/demo/services/InventoryService.java
@@ -4,11 +4,15 @@ package com.example.demo.services;
 import com.example.demo.model.Order;
 import jakarta.jms.Message;
 import jakarta.jms.ObjectMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InventoryService {
+
+    @Autowired
+    ProductService productService;
 
 
     @JmsListener(destination = "order-queue")
@@ -17,6 +21,9 @@ public class InventoryService {
                 ObjectMessage objectMessage = (ObjectMessage) message;
                 Order order = (Order) objectMessage.getObject();
                 System.out.println("Order received: " + order);
+
+                productService.dispatch(order);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
